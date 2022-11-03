873672cdcfb60f1a42152d91a70af79913a7f337
Ramith Perera <ramith@persystance.com>
2022-11-03 12:55:36 +0530

    splash screen changes and login UI done

--- lib/screens/dashboard/splash_screen.dart
+++ lib/screens/dashboard/splash_screen.dart
@@ -1,16 +1,19 @@
 //splashscreen here
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:uee_taprobane/main.dart';
+import 'package:uee_taprobane/screens/auth/login_screen.dart';
 import 'package:uee_taprobane/utils/constants.dart';
 
-class SplashSCreen extends StatefulWidget {
-  SplashSCreen({Key? key}) : super(key: key);
+class SplashScreen extends StatefulWidget {
+  SplashScreen({Key? key}) : super(key: key);
 
   @override
-  State<SplashSCreen> createState() => _SplashSCreenState();
+  State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashSCreenState extends State<SplashSCreen> {
+class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
@@ -32,5 +35,25 @@ class _SplashSCreenState extends State<SplashSCreen> {
         ),
       ),
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    startTime();
+  }
+
+  startTime() async {
+    var duration = const Duration(seconds: 3);
+    return Timer(duration, checkUser);
+  }
+
+  checkUser() {
+    Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => LoginScreen(),
+          // settings: RouteSettings(arguments: user),
+        ));
   }
 }
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -3,7 +3,8 @@
 import 'package:flutter/material.dart';
 
 const colorGreen = Color(0xff2ab934);
-const colorGreen2 = Color(0xff2ab934);
+const colorGreen2 = Color(0xff80b918);
+const colorGreen3 = Color(0xff52b788);
 
 
 const String imagePath = "assets/images/";
