202b46e97946a21a1cd52ff3918acd130d3aaa4d
Ramith Perera <ramith@persystance.com>
2022-11-03 13:21:12 +0530

    widget functions and dependency update

--- lib/utils/widget_functions.dart
+++ lib/utils/widget_functions.dart
@@ -1,15 +1,40 @@
 //common widget functions goes here
 
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:uee_taprobane/custom/progress_dialog.dart';
+import 'package:uee_taprobane/utils/constants.dart';
 
-Widget addVerticalSpace(double height){
-  return SizedBox(
-      height:height
+Widget addVerticalSpace(double height) {
+  return SizedBox(height: height);
+}
+
+Widget addHorizontalSpace(double width) {
+  return SizedBox(width: width);
+}
+
+showToastMessage(String message) {
+  Fluttertoast.showToast(
+      msg: message,
+      toastLength: Toast.LENGTH_LONG,
+      gravity: ToastGravity.BOTTOM,
+      timeInSecForIosWeb: 2,
+      backgroundColor: color33,
+      textColor: Colors.white,
+      fontSize: 16.0);
+}
+
+showLoadingDialog(BuildContext context) {
+  showDialog(
+    barrierDismissible: false,
+    context: context,
+    builder: (BuildContext context) {
+      return WillPopScope(
+          onWillPop: () async => true, child: const ProgressDialog(title: ''));
+    },
   );
 }
 
-Widget addHorizontalSpace(double width){
-  return SizedBox(
-      width:width
-  );
+void hideDialog(BuildContext context) {
+  Navigator.pop(context);
 }
