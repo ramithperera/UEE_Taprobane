a5dd849cbb28b392226cb7377aa5619518691f43
Ramith Perera <ramith@persystance.com>
2022-11-07 10:12:26 +0530

    delivery person new changes

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -230,7 +230,7 @@ class _LoginScreenState extends State<LoginScreen> {
     //   }
     // }
     Navigator.push(
-                context, MaterialPageRoute(builder: (context) => MerchantHome()));
+                context, MaterialPageRoute(builder: (context) => DeliveryPersonHome()));
         }
 }
 
